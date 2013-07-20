6399abc77d255fd96d83737bc012781c2f0f07d5
Mike Dalton <michaelcdalton@gmail.com>
2013-07-20 19:40:20 -0400

    added css and updated pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 if Rails.env.development?
-  Post.create!(title: 'First Post',
-               body: Forgery(:lorem_ipsum).words(50),
-               tags: 'first')
-  Post.create!(title: 'Another Post',
-               body: Forgery(:lorem_ipsum).words(75),
-               tags: '')
-  Post.create!(title: 'Hello, world.',
-               body: Forgery(:lorem_ipsum).words(25),
-               tags: 'hello,world')
+  Post.create!(title: Forgery(:lorem_ipsum).words(8),
+               body: Forgery(:lorem_ipsum).paragraphs(5),
+               tags: 'foo')
+  Post.create!(title: Forgery(:lorem_ipsum).words(7),
+               body: Forgery(:lorem_ipsum).paragraphs(10),
+               tags: 'foo,bar')
+  Post.create!(title: Forgery(:lorem_ipsum).words(6),
+               body: Forgery(:lorem_ipsum).paragraphs(6),
+               tags: 'bar,baz')
 end
